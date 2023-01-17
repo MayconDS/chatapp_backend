13c64e16f641d40ba6aa5910c26266ca1d0f5e8a
Maycon Douglas <crossfireelite601@gmail.com>
2023-01-17 11:47:08 -0300

    first commit

--- app.js
+++ app.js
@@ -36,7 +36,14 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(cors())
+app.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*')
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  app.use(cors())
+  next()
+})
 
 app.use('/users', usersRouter)
 app.use('/chat', chatRouter)
