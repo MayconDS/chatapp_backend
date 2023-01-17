eb6fabd3a6427001a2117b190f3446b3abe69660
Maycon Douglas <crossfireelite601@gmail.com>
2023-01-17 12:20:36 -0300

    first commit

--- app.js
+++ app.js
@@ -36,11 +36,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(
-  cors({
-    origin: '*',
-  }),
-)
+app.use(cors())
 
 app.use('/users', usersRouter)
 app.use('/chat', chatRouter)
