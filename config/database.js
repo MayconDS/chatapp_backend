1f8767ce85670b03f398e37a18c4d689477ecde2
Maycon Douglas <crossfireelite601@gmail.com>
2023-01-20 12:41:13 -0300

    first commit

--- config/database.js
+++ config/database.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config('')
 mongoose.Promise = global.Promise
 
-
-  
+mongoose.set('strictQuery', true)
 
 mongoose
   .connect(process.env.MONGO_URL, {
