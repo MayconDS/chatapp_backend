07cda81b5fd21e34a1327e9bb0cd17095a48b2d8
Maycon Douglas <crossfireelite601@gmail.com>
2023-01-21 21:36:47 -0300

    first commit

--- app.js
+++ app.js
@@ -48,7 +48,12 @@ app.use(
     credentials: true,
     methods: ['POST', 'GET', 'PUT', 'DELTE', 'OPTIONS'],
     exposedHeaders: ['Authorization'],
-    allowedHeaders: ['Authorization', 'Content-Type', 'x-access-token'],
+    allowedHeaders: [
+      'Authorization',
+      'Content-Type',
+      'x-access-token',
+      'header',
+    ],
   }),
 )
 
